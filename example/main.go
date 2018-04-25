c77c411e7c56a88d8282c40188239e6a08d7c291
padue <pascal.dueblin@usb.ch>
2018-04-25 15:45:31 +0200

    extend startrek example

--- example/main.go
+++ example/main.go
@@ -2,22 +2,46 @@
 package main
 
 import (
-	"database/sql"
+	"fmt"
 	"io/ioutil"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+
+	startrek "github.com/dkfbasel/protobuf/example/domain"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root@/startrek")
+
+	// initialize database connection
+	db, err := sqlx.Connect("mysql", "commander:123456@tcp(0.0.0.0:3333)/startrek")
 	if err != nil {
-		panic(err.Error())
+		log.Fatalln(err)
 	}
 
+	// create table starfleet and insert data
 	setupStmt, err := ioutil.ReadFile("sql/setup.sql")
 	if err != nil {
 		panic(err.Error())
 	}
+
+	// initialize a startfleet ship struct
+	starshipFleetShip := startrek.StarfleetShip{}
+
+	databaseAlias := startrek.StarfleetShipAlias(starshipFleetShip)
+
+	stmt := `
+	SELECT name, passengers, mission, departure_time_of_ship
+	FROM starfleet
+	WHERE name = "USS Enterprise"`
+
+	err = db.Get(&databaseAlias, stmt)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("%v", databaseAlias)
 
 	_, err = db.Exec(string(setupStmt))
 	if err != nil {
--- example/sql/setup.sql
+++ example/sql/setup.sql
@@ -1,12 +1,12 @@
 CREATE TABLE starfleet (
 	name TEXT NOT NULL,
 	passengers INT(6),
-	operation TEXT,
+	mission TEXT,
 	departure_time_of_ship TIMESTAMP
 );
 
-INSERT INTO starfleet (name, passengers, operation, departure_time_of_ship)
-VALUES ("USS Enterprise", 654, NULL, now();
+INSERT INTO starfleet (name, passengers, mission, departure_time_of_ship)
+VALUES ("USS Enterprise", 654, NULL, now());
 
-INSERT INTO starfleet (name, passengers, operation, departure_time_of_ship)
+INSERT INTO starfleet (name, passengers, mission, departure_time_of_ship)
 VALUES ("USS Discovery", NULL, "spore drive development", NULL);
